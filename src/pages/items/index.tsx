6817342fd78ed3ff241de23cabd31903f222cadf
tamaki-kato-123 <tamaki.a.s.h9@gmail.com>
2023-02-22 09:34:14 +0900

    最初

---
+++ src/pages/items/index.tsx
@@ -0,0 +1,13 @@
+import ItemList from "@/components/ItemList";
+import Head from "next/head";
+import Link from "next/link";
+
+export function default List () {
+    return(
+        <>
+         <Head>
+            <title>インテリア・家具通販【bridge】</title>
+         </Head>
+        </>
+    )
+}
