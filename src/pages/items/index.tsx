4a64855d178d16c888dbdff0bbe43978fabe6e62
tamaki-kato-123 <tamaki.a.s.h9@gmail.com>
2023-02-24 02:21:05 +0900

    footer作成

--- src/pages/items/index.tsx
+++ src/pages/items/index.tsx
@@ -1,8 +1,8 @@
 import ItemList from '@/components/ItemList';
 import Head from 'next/head';
 import Link from 'next/link';
-// import Header from ;
-// import Footer from ;
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default function List() {
   return (
@@ -10,8 +10,9 @@ export default function List() {
       <Head>
         <title>インテリア・家具通販【bridge】</title>
       </Head>
-      {/* <Header /> */}
+      <Header />
       <main>
+        <div role="dialog"></div>
         <nav>
           <ol className="breadcrumbs">
             <li>
@@ -31,12 +32,13 @@ export default function List() {
 
         <fieldset>
           <form>
-            <div>
+            <div className="flex gap-10">
               <input
                 type="text"
                 name="keyword"
-                placeholder="キーワードを入力してください"
+                placeholder="キーワードを入力"
               />
+              <button type="submit">検索</button>
             </div>
           </form>
 
@@ -55,66 +57,70 @@ export default function List() {
              </div>
 
             <div className='category'>
-              <div>
-                <input type="checkbox" id="chair" name="chair" checked />
-                <label htmlFor="chair">椅子</label>
-              </div>
-              <div>
-                <input type="checkbox" id="table" name="table" />
-                <label htmlFor="table">テーブル</label>
-              </div>
-              <div>
-                <input type="checkbox" id="curtain" name="curtain" />
-                <label htmlFor="curtain">カーテン</label>
-              </div>
-              <div>
-                <input type="checkbox" id="rug" name="rug" />
-                <label htmlFor="rug">カーペット/ラグ</label>
-              </div>
-              <div>
-                <input type="checkbox" id="sofa" name="sofa" />
-                <label htmlFor="sofa">ソファ</label>
-              </div>
-              <div>
-                <input type="checkbox" id="chest" name="chest" />
-                <label htmlFor="chest">収納棚</label>
-              </div>
-              <div>
-                <input type="checkbox" id="light" name="light" />
-                <label htmlFor="light">照明</label>
-              </div>
-              <div>
-                <input type="checkbox" id="bed" name="bed" />
-                <label htmlFor="bed">ベッド/寝具</label>
-              </div>
-              <div>
-                <input type="checkbox" id="accessory" name="accessory" />
-                <label htmlFor="accessory">小物/雑貨</label>
-              </div>
+                <ul>
+                    <li>
+                        <input type="checkbox" id="chair" name="chair" checked />
+                        <label htmlFor="chair">椅子</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="table" name="table" />
+                        <label htmlFor="table">テーブル</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="curtain" name="curtain" />
+                        <label htmlFor="curtain">カーテン</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="rug" name="rug" />
+                        <label htmlFor="rug">カーペット/ラグ</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="sofa" name="sofa" />
+                        <label htmlFor="sofa">ソファ</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="chest" name="chest" />
+                        <label htmlFor="chest">収納棚</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="light" name="light" />
+                        <label htmlFor="light">照明</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="bed" name="bed" />
+                        <label htmlFor="bed">ベッド/寝具</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="accessory" name="accessory" />
+                        <label htmlFor="accessory">小物/雑貨</label>
+                    </li>
+                </ul>
             </div>
 
             <div className='genre'>
-              <div>
-                <input type="checkbox" id="nordic" name="nordic" checked/>
-                <label htmlFor="nordic">北欧風</label>
-              </div>
-              <div>
-                <input type="checkbox" id="natural" name="natural" />
-                <label htmlFor="natural">ナチュラル</label>
-              </div>
-              <div>
-                <input type="checkbox" id="jmodern" name="jmodern" />
-                <label htmlFor="jmodern">和モダン</label>
-              </div>
-              <div>
-                <input type="checkbox" id="feminine" name="feminine" />
-                <label htmlFor="feminine">フェミニン</label>
-              </div>
+                <ul>
+                    <li>
+                        <input type="checkbox" id="nordic" name="nordic" checked/>
+                        <label htmlFor="nordic">北欧風</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="natural" name="natural" />
+                        <label htmlFor="natural">ナチュラル</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="jmodern" name="jmodern" />
+                        <label htmlFor="jmodern">和モダン</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" id="feminine" name="feminine" />
+                        <label htmlFor="feminine">フェミニン</label>
+                    </li>
+                </ul>
             </div>
           </form>
         </fieldset>
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
