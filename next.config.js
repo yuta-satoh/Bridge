efb3a2177a88cd61f92011b729c822b0ba2e6b58
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-02-24 10:40:42 +0900

    next.config.jsの編集 "/api/..."で"http://localhost:8000/..."にアクセス出来ます。

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/api',
+        destination: 'http://localhost:8000',
+      },
+      {
+        source: '/api/:slug',
+        destination: 'http://localhost:8000/:slug',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
