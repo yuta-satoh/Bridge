a97f0a24fa18927deda1936a23e7556e4dc587fe
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-02-27 16:44:46 +0900

    src/libにcookieの値を取得するgetCookieArray関数を定義

---
+++ src/lib/getCookieArray.ts
@@ -0,0 +1,12 @@
+export default function getCookieArray() {
+  /// 参照したいcookieのキー名を格納
+  const key = 'id';
+
+  /// 正規表現でcookie値を取得
+//   const value = document.cookie.match(
+// //     new RegExp(key + '=([^;]*);*')
+// //   )[1];
+
+//   /// cookie値を表示
+//   console.log(value);
+}
