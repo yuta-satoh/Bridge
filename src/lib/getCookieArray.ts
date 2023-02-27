500b677ecc18cf96fe2757d79a43cedeb795a071
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-02-27 17:48:51 +0900

    /lib/getCookiesArray.tsの作成

--- src/lib/getCookieArray.ts
+++ src/lib/getCookieArray.ts
@@ -1,12 +1,14 @@
-export default function getCookieArray() {
-  /// 参照したいcookieのキー名を格納
-  const key = 'id';
-
-  /// 正規表現でcookie値を取得
-//   const value = document.cookie.match(
-// //     new RegExp(key + '=([^;]*);*')
-// //   )[1];
-
-//   /// cookie値を表示
-//   console.log(value);
+export default function getCookieValue(): string {
+  // cookie文字列をセミコロン(;)で分割
+  const cookies = document.cookie.split(';');
+  // 配列cookiesを=で分割して代入し、
+  // 0番目の要素が"id"なら1番目の要素(cookieの値)を返す
+  for (let cookie of cookies) {
+      const cookiesArray = cookie.split('='); 
+      if (cookiesArray[0].trim() === "id") { 
+          return cookiesArray[1];  // (key[0],value[1])
+      }
+  }
+  // 上記の処理で何もリターンされなければ空文字を返す
+  return '';
 }
