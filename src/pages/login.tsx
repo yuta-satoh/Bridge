54f8daefef4843d33e5e37ea959ce1da4a3dbde7
yuta-satoh <yuta.sato@rakus-partners.co.jp>
2023-02-27 11:11:12 +0900

    create module css & error css

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import userRegisterStyles from '../styles/userRegister.module.css';
+import urStyles from '../styles/userRegister.module.css';
 
 export default function userRegister() {
   return (
@@ -8,128 +8,61 @@ export default function userRegister() {
       <Head>
         <title>ログイン</title>
       </Head>
-      <style jsx>{`
-        main {
-          background-color: ;
-          background-image: url('/images/background_login/backgroundimage.jpeg');
-          background-size: cover;
-          width: 100%;
-          max-height: 100%;
-          display: flex;
-        }
-        .body {
-          background-color: rgba(255, 255, 255, 0.9);
-          margin: 50px auto;
-          height: 900px;
-          width: 45%;
-        }
-        .title {
-          text-align: center;
-          border-bottom: 2px black solid;
-          max-width: 425px;
-          margin: 50px auto;
-        }
-        form {
-          padding: 5px 100px;
-          margin: auto;
-        }
-        h1 {
-          font-size: 30px;
-        }
-        p {
-          font-size: 12px;
-          padding-bottom: 15px;
-        }
-        input {
-          border: 1px grey solid;
-          height: 40px;
-          margin: 5px 5px;
-          padding: 0 10px;
-        }
-        .inputItems {
-          padding-bottom: 10px;
-          padding-left: 25px;
-        }
-        label {
-          font-size: 13px;
-          margin-bottom: 5px;
-        }
-        .inputParts {
-          width: 100%;
-        }
-        .buttonArea {
-          text-align: center;
-          margin-top: 30px;
-        }
-        .loginLink {
-          margin-top: 40px;
-          text-align: center;
-          margin: auto;
-        }
-        .subTitle {
-          font-size: 25px;
-          padding-bottom: 15px;
-        }
-        .submitButton {
-          background-color: black;
-          color: white;
-          font-weight: bold;
-          padding: 20px 60px;
-          font-size: 17px;
-        }
-        .buttonSpan {
-          padding-left: 15px;
-        }
-        .linkButton {
-          border: 2.3px black solid;
-          padding: 8px 60px;
-          background-color: white;
-          font-size: 13px;
-          font-weight: bold;
-        }
-      `}</style>
-      <main>
-        <div className="body">
-          <div className="title">
-            <h1>LOGIN</h1>
-            <p>ログイン</p>
+      <main className={urStyles.main}>
+        <div className={urStyles.body}>
+          <div className={urStyles.title}>
+            <h1 className={urStyles.h1}>LOGIN</h1>
+            <p className={urStyles.p}>ログイン</p>
           </div>
-          <form>
-            <div className="inputItems">
-              <label htmlFor="email">メールアドレス</label>
+          <form className={urStyles.form}>
+            <div className={urStyles.inputItems}>
+              <label htmlFor="email" className={urStyles.label}>
+                メールアドレス
+              </label>
               <br />
               <input
                 type="email"
                 name="email"
                 id="email"
-                className="inputParts border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.inputParts} border border-neutral-500 rounded pl-2.5`}
                 placeholder="例：bridge@example.com"
               />
+              <p className={urStyles.nope}>
+                ※メールアドレスを入力して下さい
+              </p>
             </div>
-            <div className="inputItems">
-              <label htmlFor="password">パスワード</label>
+            <div className={urStyles.inputItems}>
+              <label htmlFor="password" className={urStyles.label}>
+                パスワード
+              </label>
               <br />
               <input
                 type="password"
                 name="password"
                 id="password"
-                className="inputParts border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.inputParts} border border-neutral-500 rounded pl-2.5`}
                 placeholder="例：abcdef123456"
               />
+              <p className={urStyles.nope}>
+                ※パスワードを入力して下さい
+              </p>
             </div>
-            <div className="buttonArea">
-              <button type="button" className="submitButton">
-                ログイン<span className="buttonSpan">→</span>
+            <div className={urStyles.buttonArea}>
+              <button type="button" className={urStyles.loginButton}>
+                ログイン<span className={urStyles.buttonSpan}>→</span>
               </button>
             </div>
           </form>
-          <div className="title">
-            <h1 className="subTitle">はじめてのお客様はこちら</h1>
+          <div className={urStyles.title}>
+            <h2 className={`${urStyles.subTitle}`}>
+              はじめてご利用のお客様はこちら
+            </h2>
           </div>
-          <div className="loginLink">
+          <div className={urStyles.loginLink}>
             <Link href="/userRegister">
-              <button type="button" className="linkButton">
-                新規会員登録<span className="buttonSpan">→</span>
+              <button type="button" className={urStyles.linkButton}>
+                新規会員登録
+                <span className={urStyles.buttonSpan}>→</span>
               </button>
             </Link>
           </div>
--- src/pages/userRegister.tsx
+++ src/pages/userRegister.tsx
@@ -1,6 +1,7 @@
+import { url } from 'inspector';
 import Head from 'next/head';
 import Link from 'next/link';
-import userRegisterStyles from '../styles/userRegister.module.css';
+import urStyles from '../styles/userRegister.module.css';
 
 export default function userRegister() {
   return (
@@ -8,221 +9,124 @@ export default function userRegister() {
       <Head>
         <title>新規会員登録</title>
       </Head>
-      <style jsx>{`
-        main {
-          background-color: ;
-          background-image: url('/images/background_login/backgroundimage.jpeg');
-          background-size: cover;
-          width: 100%;
-          max-height: 100%;
-          display: flex;
-        }
-        .body {
-          background-color: rgba(255, 255, 255, 0.9);
-          margin: 50px auto;
-          height: 1500px;
-          width: 45%;
-        }
-        .title {
-          text-align: center;
-          border-bottom: 2px black solid;
-          max-width: 425px;
-          margin: 50px auto;
-        }
-        form {
-          padding: 5px 100px;
-          margin: auto;
-        }
-        h1 {
-          font-size: 30px;
-        }
-        p {
-          font-size: 12px;
-          padding-bottom: 15px;
-        }
-        input {
-          border: 1px grey solid;
-          height: 40px;
-          margin: 5px 5px;
-          padding: 0 10px;
-        }
-        .inputItems {
-          padding-bottom: 10px;
-          padding-left: 25px;
-        }
-        .number {
-          width: 110px;
-        }
-        label {
-          font-size: 13px;
-          margin-bottom: 5px;
-        }
-        .genderLabel {
-          font-size: 17px;
-          margin: 0px;
-        }
-        .inputParts {
-          width: 100%;
-        }
-        .zipButton {
-          border: 1px black solid;
-          font-size: 11px;
-          background-color: black;
-          padding: 8px 20px;
-          color: white;
-          font-weight: bold;
-        }
-        .buttonArea {
-          text-align: center;
-          margin-top: 30px;
-        }
-        .primary {
-          font-size: 7px;
-          margin-left: 5px;
-          padding: 3px 9px;
-          color: red;
-          border: 1px red solid;
-        }
-        .loginLink {
-          margin-top: 40px;
-          text-align: center;
-          margin: auto;
-        }
-        .subTitle {
-          font-size: 25px;
-          padding-bottom: 15px;
-        }
-        .submitButton {
-          background-color: black;
-          color: white;
-          font-weight: bold;
-          padding: 20px 60px;
-          font-size: 17px;
-        }
-        .buttonSpan {
-          padding-left: 15px;
-        }
-        .linkButton {
-          border: 2.3px black solid;
-          padding: 8px 60px;
-          background-color: white;
-          font-size: 13px;
-          font-weight: bold;
-        }
-        .radioField {
-          display: flex;
-          justify-content: space-around;
-          margin-top: 10px;
-        }
-        .radioForm {
-          display: flex;
-          align-items: center;
-          border: 1px grey solid;
-          padding: 5px 20px;
-          background-color: white;
-        }
-        .radioButton {
-          width: 17px;
-        }
-        .clearButton {
-          background-color: white;
-          color: black;
-          font-weight: bold;
-          border: 2.3px black solid;
-          padding: 10px 40px;
-          font-size: 14px;
-        }
-      `}</style>
-      <main>
-        <div className="body">
-          <div className="title">
-            <h1>JOIN TO Bridge</h1>
-            <p>Bridge会員に登録する</p>
+      <main className={urStyles.main}>
+        <div className={urStyles.urbody}>
+          <div className={urStyles.title}>
+            <h1 className={urStyles.h1}>JOIN TO Bridge</h1>
+            <p className={urStyles.p}>Bridge会員に登録する</p>
           </div>
-          <form>
-            <div className="inputItems">
-              <label htmlFor="lastName">お名前</label>
-              <span className="primary">必須</span>
+          <form className={urStyles.form}>
+            <div className={urStyles.inputItems}>
+              <label htmlFor="lastName" className={urStyles.label}>
+                お名前
+              </label>
+              <span className={urStyles.primary}>必須</span>
               <br />
               <input
                 type="text"
                 name="lastName"
                 id="lastName"
                 placeholder="例：山田"
-                className="border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.input} border border-neutral-500 rounded pl-2.5`}
               />
               <input
                 type="text"
                 name="firstName"
                 id="firstName"
                 placeholder="例：花子"
-                className="border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.input} border border-neutral-500 rounded pl-2.5`}
               />
+              <p className={urStyles.nope}>※名前を入力して下さい</p>
             </div>
-            <div className="inputItems">
-              <label htmlFor="female">性別</label>
-              <span className="primary">必須</span>
+            <div className={urStyles.inputItems}>
+              <label htmlFor="female" className={urStyles.label}>
+                性別
+              </label>
+              <span className={urStyles.primary}>必須</span>
               <br />
-              <div className="radioField">
-                <div className="radioForm border-neutral-500 rounded pl-2.5">
+              <div className={urStyles.radioField}>
+                <div
+                  className={`${urStyles.radioForm} border border-neutral-500 rounded pl-2.5`}
+                >
                   <input
                     type="radio"
                     name="gender"
                     id="female"
                     value="female"
-                    className="radioButton"
+                    className={urStyles.radioButton}
                   />
-                  <label htmlFor="female" className="genderLabel">
+                  <label
+                    htmlFor="female"
+                    className={urStyles.genderLabel}
+                  >
                     女性&emsp;
                   </label>
                 </div>
-                <div className="radioForm border-neutral-500 rounded pl-2.5">
+                <div
+                  className={`${urStyles.radioForm} border border-neutral-500 rounded pl-2.5`}
+                >
                   <input
                     type="radio"
                     name="gender"
                     id="male"
                     value="male"
-                    className="radioButton"
+                    className={urStyles.radioButton}
                   />
-                  <label htmlFor="male" className="genderLabel">
+                  <label
+                    htmlFor="male"
+                    className={urStyles.genderLabel}
+                  >
                     男性&emsp;
                   </label>
                 </div>
-                <div className="radioForm border-neutral-500 rounded pl-2.5">
+                <div
+                  className={`${urStyles.radioForm} border border-neutral-500 rounded pl-2.5`}
+                >
                   <input
                     type="radio"
                     name="gender"
                     id="other"
                     value="other"
-                    className="radioButton"
+                    className={urStyles.radioButton}
                   />
-                  <label htmlFor="other" className="genderLabel">
+                  <label
+                    htmlFor="other"
+                    className={urStyles.genderLabel}
+                  >
                     その他
                   </label>
                 </div>
               </div>
+              <p className={urStyles.nope}>※性別を選択して下さい</p>
             </div>
-            <div className="inputItems">
-              <label htmlFor="email">メールアドレス</label>
-              <span className="primary">必須</span>
+            <div className={urStyles.inputItems}>
+              <label htmlFor="email" className={urStyles.label}>
+                メールアドレス
+              </label>
+              <span className={urStyles.primary}>必須</span>
               <br />
               <input
                 type="email"
                 name="email"
                 id="email"
-                className="inputParts border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.inputParts} border border-neutral-500 rounded pl-2.5`}
                 placeholder="例：bridge@example.com"
               />
+              <p className={urStyles.nope}>
+                ※メールアドレスを入力して下さい
+              </p>
             </div>
-            <div className="inputItems">
-              <label htmlFor="tell">電話番号</label>
-              <span className="primary">必須</span>
+            <div className={urStyles.inputItems}>
+              <label htmlFor="tell" className={urStyles.label}>
+                電話番号
+              </label>
+              <span className={urStyles.primary}>必須</span>
               <br />
               <input
                 type="tell"
                 name="tell"
                 id="tell"
-                className="number border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.number} border border-neutral-500 rounded pl-2.5`}
                 placeholder="xxx"
               />
               <span>-</span>
@@ -230,7 +134,7 @@ export default function userRegister() {
                 type="tell"
                 name="tell"
                 id="tell2"
-                className="number border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.number} border border-neutral-500 rounded pl-2.5`}
                 placeholder="xxxx"
               />
               <span>-</span>
@@ -238,20 +142,25 @@ export default function userRegister() {
                 type="tell"
                 name="tell"
                 id="tell3"
-                className="number border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.number} border border-neutral-500 rounded pl-2.5`}
                 placeholder="xxxx"
               />
+              <p className={urStyles.nope}>
+                ※電話番号を入力して下さい
+              </p>
             </div>
-            <div className="inputItems">
-              <label htmlFor="zipcode">郵便番号</label>
-              <span className="primary">必須</span>
+            <div className={urStyles.inputItems}>
+              <label htmlFor="zipcode" className={urStyles.label}>
+                郵便番号
+              </label>
+              <span className={urStyles.primary}>必須</span>
               <br />
               <input
                 type="text"
                 pattern="^[0-9]+$"
                 name="zipcode"
                 id="zipcode"
-                className="number border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.number} border border-neutral-500 rounded pl-2.5`}
                 placeholder="xxx"
               />
               <span>-</span>
@@ -260,72 +169,96 @@ export default function userRegister() {
                 pattern="^[0-9]+$"
                 name="zipcode"
                 id="zipcode2"
-                className="number border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.number} border border-neutral-500 rounded pl-2.5`}
                 placeholder="xxxx"
               />
-              <button className="zipButton text-white bg-neutral-900 border border-neutral-900 rounded px-1">
+              <button
+                className={`${urStyles.zipButton} text-white bg-neutral-900 border border-neutral-900 rounded px-1`}
+              >
                 住所検索
               </button>
+              <p className={urStyles.nope}>
+                ※郵便番号を入力して下さい
+              </p>
             </div>
-            <div className="inputItems">
-              <label htmlFor="address">住所</label>
-              <span className="primary">必須</span>
+            <div className={urStyles.inputItems}>
+              <label htmlFor="address" className={urStyles.label}>
+                住所
+              </label>
+              <span className={urStyles.primary}>必須</span>
               <br />
               <input
                 type="text"
                 name="address"
                 id="address"
-                className="inputParts border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.inputParts} border border-neutral-500 rounded pl-2.5`}
                 placeholder="例：東京都新宿区新宿2丁目x-x-x"
               />
+              <p className={urStyles.nope}>※住所を入力して下さい</p>
             </div>
-            <div className="inputItems">
-              <label htmlFor="password">パスワード</label>
-              <span className="primary">必須</span>
+            <div className={urStyles.inputItems}>
+              <label htmlFor="password" className={urStyles.label}>
+                パスワード
+              </label>
+              <span className={urStyles.primary}>必須</span>
               <br />
               <input
                 type="password"
                 name="password"
                 id="password"
-                className="inputParts border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.inputParts} border border-neutral-500 rounded pl-2.5`}
                 placeholder="例：abcdef123456"
               />
-              <p>※8〜20文字で入力してください</p>
+              <p className={urStyles.nope}>
+                ※パスワードを入力して下さい
+              </p>
+              <p className={urStyles.p}>
+                ※8〜20文字で入力してください
+              </p>
             </div>
-            <div className="inputItems">
-              <label htmlFor="confirmationPassword">
+            <div className={urStyles.inputItems}>
+              <label
+                htmlFor="confirmationPassword"
+                className={urStyles.label}
+              >
                 パスワード確認用
               </label>
-              <span className="primary">必須</span>
+              <span className={urStyles.primary}>必須</span>
               <br />
               <input
                 type="password"
                 name="confirmationPassword"
                 id="confirmationPassword"
-                className="inputParts border border-neutral-500 rounded pl-2.5"
+                className={`${urStyles.inputParts} border border-neutral-500 rounded pl-2.5`}
                 placeholder="例：abcdef123456"
               />
-              <p>※確認のためパスワードを再入力して下さい</p>
+              <p className={urStyles.nope}>
+                ※パスワード確認用を入力してください
+              </p>
+              <p className={urStyles.p}>
+                ※確認のためパスワードを再入力して下さい
+              </p>
               <br />
             </div>
-            <div className="buttonArea">
-              <button type="button" className="submitButton">
-                会員登録をする<span className="buttonSpan">→</span>
+            <div className={urStyles.buttonArea}>
+              <button type="button" className={urStyles.submitButton}>
+                会員登録をする
+                <span className={urStyles.buttonSpan}>→</span>
               </button>
             </div>
-            <div className="buttonArea">
-              <button type="button" className="clearButton">
+            <div className={urStyles.buttonArea}>
+              <button type="button" className={urStyles.clearButton}>
                 入力クリア
               </button>
             </div>
           </form>
-          <div className="title">
-            <h1 className="subTitle">会員の方はこちら</h1>
+          <div className={urStyles.title}>
+            <h2 className={urStyles.subTitle}>会員の方はこちら</h2>
           </div>
-          <div className="loginLink">
+          <div className={urStyles.loginLink}>
             <Link href="/login">
-              <button type="button" className="linkButton">
-                ログイン<span className="buttonSpan">→</span>
+              <button type="button" className={urStyles.linkButton}>
+                ログイン<span className={urStyles.buttonSpan}>→</span>
               </button>
             </Link>
           </div>
