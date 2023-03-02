24ab87b79ebf92cc7357f6532cefe60026ac7b0d
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-02 17:57:03 +0900

    ReviewList.tsx追記 サンプルレビューデータを修正

--- src/components/ReviewList.tsx
+++ src/components/ReviewList.tsx
@@ -1,1 +1,56 @@
-import useSWR from 'swr';
+import useSWR, { Fetcher } from 'swr';
+
+type Reviews = {
+  id: number;
+  item_id: number;
+  user_id: number;
+  anonymous: boolean;
+  evaluation: string;
+  title: string;
+  description: string;
+  date: Date;
+  delete: boolean;
+};
+
+const fetcher: Fetcher<Reviews[], string> = (args: string) =>
+  fetch(args).then((res) => res.json());
+
+export default function ReviewList({ itemId }: { itemId: string }) {
+  const { data, error } = useSWR<Reviews[], Error>(
+    `http://127.0.0.1:8000/reviews?item_id=eq.${itemId}`,
+    fetcher
+  );
+
+  if (error) {
+    return (
+      <>
+        <div className="reviwsArea">
+          <div className="background">
+            <p>レビューはまだ投稿されていません</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+  if (!data) {
+    return (
+      <>
+        <div className="reviwsArea">
+          <div className="background">
+            <p>レビューはまだ投稿されていません</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="reviwsArea">
+        <div className="background">
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+}
