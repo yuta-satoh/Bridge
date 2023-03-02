dd2132a21f651b3b4686dca4b6f897d22be69b60
yuta-satoh <yuta.sato@rakus-partners.co.jp>
2023-03-02 11:13:50 +0900

    header error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,24 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [auth, setAuth] = useState<boolean>();
+  useEffect(() => {
+    const status = Cookies.get('status');
+    if (status === 'true' && auth === true) {
+      return;
+    } else if (status === 'true' && auth !== true) {
+      setAuth(true);
+    } else {
+      setAuth(false); 
+    }
+  });
   return (
     <>
-      <Header />
+      <Header auth={auth}/>
       <Component {...pageProps} />
       <Footer/>
     </>
