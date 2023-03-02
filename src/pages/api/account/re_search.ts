7e79ea6773313f4c18a1984c59d869878421da72
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-02 10:11:00 +0900

    自分のid以外でemail/passwordの重複をチェックする api/account/re_search.tsを作成

---
+++ src/pages/api/account/re_search.ts
@@ -0,0 +1,60 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type User = {
+    lastName: string,
+    firstName: string,
+    gender: string,
+    tell: string,
+    email: string,
+    zipcode: string,
+    address: string,
+    password: string,
+    delete: boolean,
+  }
+
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYXBpX3VzZXIifQ.OOP7yE5O_2aYFQG4bgMBQ9r0f9sikNqXbhJqoS9doTw";
+
+const searchUser = async (params: {
+  cookie: string;
+  email: string,
+  password: string
+}) => {
+  const searchEmailResult = await fetch(`http://127.0.0.1:8000/users?email=eq.${params.email}`, {
+    method: "GET",
+    headers: {
+            "Authorization": `Bearer ${TOKEN}`,
+            "Content-Type": "application/json",
+    },
+	}).then((res) => res.json()).then((data) => {
+    if (data.length !== 0) {
+      return "このメールアドレスはすでに使用されています"
+    } 
+    return ""
+  })
+
+  const searchPasswordResult = await fetch(`http://127.0.0.1:8000/users?password=eq.${params.password}`, {
+    method: "GET",
+    headers: {
+            "Authorization": `Bearer ${TOKEN}`,
+            "Content-Type": "application/json",
+    },
+	}).then((res) => res.json()).then((data) => {
+    if (data.length !== 0) {
+      return "このパスワードはすでに使用されています"
+    }
+    return ""
+  })
+  return {
+    email: searchEmailResult,
+    password: searchPasswordResult,
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email, password, cookie } = req.body;
+  const searchResult = await searchUser({email, password, cookie})
+  res.status(200).json(searchResult)
+} 
