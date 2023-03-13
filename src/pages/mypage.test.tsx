e4d104d12640dded9339c5a8f58c9f14f7e05ecb
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-13 17:51:16 +0900

    カートに商品がない場合のmypage.tsxのスナップショットテスト（成功）

--- src/pages/mypage.test.tsx
+++ src/pages/mypage.test.tsx
@@ -1,11 +1,10 @@
 import renderer from 'react-test-renderer';
 import Mypage from './mypage';
 
-
-
 // テスト用のクッキーの値を定義
 const testCookieValue = '1';
-describe('mypage.tsxの描画テスト', () => {
+describe('mypage.tsxのスナップショットテスト', () => {
+  // カート画像ロード中のスナップショットテスト
   it('ロード中', () => {
     // cookieValueにテスト用のクッキーの値を格納
     const cookieValue = testCookieValue;
@@ -16,10 +15,32 @@ describe('mypage.tsxの描画テスト', () => {
     // Mypageコンポーネントにpropsを渡す
     const mypage_component = <Mypage {...props} />;
 
-    // 描画テスト
+    // スナップショットテスト
     const tree = renderer.create(mypage_component).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
+  //   カートに商品がない場合のスナップショットテスト
+  it('カートに商品がありません', () => {
+    // CurrentCartItemsコンポーネントをモックし、カートに商品がない場合のコンポーネントを返す
+    jest.mock('../components/CurrentCartItems', () => {
+      return (
+        <>
+          <div className="container">
+            <div className="background">
+              <p>カートに商品がありません</p>
+            </div>
+          </div>
+        </>
+      );
+    });
+    // スナップショットテスト手続きは上に同じ
+    const cookieValue = testCookieValue;
+    const props = { cookieValue };
+    const mypage_component = <Mypage {...props} />;
+    const tree = renderer.create(mypage_component).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
+  //カートに商品がある場合の
 });
