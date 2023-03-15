237b7b490d5851fd353765ad1dbfd1479246635f
YukiNakajima <yuki.nakajima@rakus-partners.co.jp>
2023-03-15 14:18:22 +0900

    userResisterフォルダのapi取得をsupabase用に変更

--- src/pages/api/userResister/createCart.ts
+++ src/pages/api/userResister/createCart.ts
@@ -4,17 +4,25 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-	const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYXBpX3VzZXIifQ.OOP7yE5O_2aYFQG4bgMBQ9r0f9sikNqXbhJqoS9doTw";
+	// const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYXBpX3VzZXIifQ.OOP7yE5O_2aYFQG4bgMBQ9r0f9sikNqXbhJqoS9doTw";
 
     const email = req.query.email
 
-    const uid: { id: number }[] = await (await fetch(`http://127.0.0.1:8000/users?select=id&email=eq.${email}`)).json()
+    const uid: { id: number }[] = await (await fetch(`${process.env.SUPABASE_URL}/users?select=id&email=eq.${email}`, {
+        method: "GET",
+        headers: {
+            "apikey": `${process.env.SUPABASE_API_KEY}`,
+            "Authorization": `Bearer ${process.env.SUPABASE_API_KEY}`,
+            "Content-Type": "application/json",
+        },
+    })).json()
   
-    const response = await fetch("http://127.0.0.1:8000/carts", {
-            method: req.method,
-            headers: {
-        "Authorization": `Bearer ${TOKEN}`,
-        "Content-Type": "application/json",
+    const response = await fetch(`${process.env.SUPABASE_URL}/carts`, {
+        method: req.method,
+        headers: {
+            "apikey": `${process.env.SUPABASE_API_KEY}`,
+            "Authorization": `Bearer ${process.env.SUPABASE_API_KEY}`,
+            "Content-Type": "application/json",
         },
             body: JSON.stringify({ user_id: uid[0].id })
         })
