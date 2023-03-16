9e3f7741b1b96522173750ab50dbb76e45d2761c
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-16 17:55:09 +0900

    グローバルAPIキーの設定

--- src/pages/api/stripe.ts
+++ src/pages/api/stripe.ts
@@ -1,1 +1,15 @@
-import { Stripe } from "stripe";
+import Stripe from "stripe";
+import { NextApiRequest, NextApiResponse } from "next";
+
+// グローバルAPI認証キーの設定
+// new Stripe('api_key', 'stripe_version etc...')
+const stripe = new Stripe(`${process.env.STRIPE_TEST_API_KEY}`, { apiVersion: '2022-11-15'});
+
+
+export async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+){
+    // 一旦決済機能だけ先に実装
+
+};
