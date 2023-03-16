ce215437042a8b92583d22575e6ac97c63c66b40
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-16 09:01:10 +0900

    password/index.tsxのCSSをモジュール化

--- src/pages/account/password/index.tsx
+++ src/pages/account/password/index.tsx
@@ -6,6 +6,7 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import cModule from '../../../styles/coordination.module.css';
 import urStyles from '../../../styles/userRegister.module.css';
 import Auth from '../../auth/auth';
+import passModule from '../../../styles/account/password/password.module.css';
 
 // userデータの型を定義
 type User = {
@@ -197,139 +198,9 @@ export default function Password({
       <Head>
         <title>パスワードの変更</title>
       </Head>
-      <style jsx>{`
-        main {
-          background-color: ;
-          background-size: cover;
-          width: 100%;
-          max-height: 100%;
-          display: flex;
-        }
-        section {
-          background-color: rgba(255, 255, 255, 0.9);
-          margin: 50px auto;
-          width: 70%;
-        }
-        .title {
-          text-align: center;
-          border-bottom: 2px black solid;
-          max-width: 425px;
-          margin: 50px auto;
-        }
-        form {
-          padding: 5px 100px;
-          margin: auto;
-          border: 1px solod black;
-        }
-        h1 {
-          font-size: 30px;
-        }
-        p {
-          font-size: 12px;
-          padding-bottom: 15px;
-        }
-        input {
-          border: 1px grey solid;
-          height: 40px;
-          margin: 5px 5px;
-          padding: 0 10px;
-        }
-        .inputItems {
-          padding-bottom: 10px;
-          padding-left: 25px;
-        }
-        .number {
-          width: 110px;
-        }
-        label {
-          font-size: 13px;
-          margin-bottom: 5px;
-        }
-        .genderLabel {
-          font-size: 17px;
-          margin: 0px;
-        }
-        .inputParts {
-          width: 100%;
-        }
-        .zipButton {
-          border: 1px black solid;
-          font-size: 11px;
-          background-color: black;
-          padding: 8px 20px;
-          color: white;
-          font-weight: bold;
-        }
-        .buttonArea {
-          text-align: center;
-          margin-top: 30px;
-        }
-        .primary {
-          font-size: 7px;
-          margin-left: 5px;
-          padding: 3px 9px;
-          color: red;
-          border: 1px red solid;
-        }
-        .loginLink {
-          margin-top: 40px;
-          text-align: center;
-          margin: auto;
-        }
-        .subTitle {
-          font-size: 25px;
-          padding-bottom: 15px;
-        }
-        .submitButton {
-          background-color: black;
-          color: white;
-          font-weight: bold;
-          padding: 20px 60px;
-          font-size: 17px;
-        }
-        .buttonSpan {
-          padding-left: 15px;
-        }
-        .linkButton {
-          border: 2.3px black solid;
-          padding: 8px 60px;
-          background-color: white;
-          font-size: 13px;
-          font-weight: bold;
-        }
-        .radioField {
-          display: flex;
-          justify-content: space-around;
-          margin-top: 10px;
-        }
-        .radioForm {
-          display: flex;
-          align-items: center;
-          border: 1px grey solid;
-          padding: 5px 20px;
-          background-color: white;
-        }
-        .radioButton {
-          width: 17px;
-        }
-        .clearButton {
-          background-color: white;
-          color: black;
-          font-weight: bold;
-          border: 2.3px black solid;
-          padding: 10px 40px;
-          font-size: 14px;
-        }
-        .completeText {
-          color: red;
-          margin-top: 10px;
-          font-size: 15px;
-          font-weight: bold;
-        }
-      `}</style>
       <Auth>
-        <main>
-          <section>
+        <main className={passModule.main}>
+          <section className={passModule.section}>
             <div>
               <ol className={cModule.links} id="top">
                 <li className={cModule.pageLink}>
@@ -343,32 +214,32 @@ export default function Password({
                 <li className={cModule.pageLink}>パスワードの変更</li>
               </ol>
             </div>
-            <div className="title">
+            <div className={`${passModule.title}`}>
               <h1>パスワードの変更</h1>
             </div>
             <form onSubmit={(e) => handleSubmit(e)}>
-              <div className="inputItems">
+              <div className={passModule.inputItems}>
                 <label htmlFor="password">現在のパスワード</label>
-                <span className="primary">必須</span>
+                <span className={passModule.primary}>必須</span>
                 <br />
                 <input
                   type="password"
                   name="password"
                   id="password"
-                  className="inputParts border border-neutral-500 rounded pl-2.5"
+                  className={`${passModule.inputParts} border border-neutral-500 rounded pl-2.5`}
                   onChange={(e) => handleChange(e)}
                 />
                 <p className={urStyles.error}>{errorText.password}</p>
               </div>
-              <div className="inputItems">
+              <div className={passModule.inputItems}>
                 <label htmlFor="newPassword">新しいパスワード</label>
-                <span className="primary">必須</span>
+                <span className={passModule.primary}>必須</span>
                 <br />
                 <input
                   type="password"
                   name="newPassword"
                   id="newPassword"
-                  className="inputParts border border-neutral-500 rounded pl-2.5"
+                  className={`${passModule.inputParts} border border-neutral-500 rounded pl-2.5`}
                   placeholder="例：abcdef123456"
                   onChange={(e) => handleChange(e)}
                 />
@@ -377,17 +248,17 @@ export default function Password({
                 </p>
                 <p>※8〜20文字で入力してください</p>
               </div>
-              <div className="inputItems">
+              <div className={passModule.inputItems}>
                 <label htmlFor="confirmationPassword">
                   パスワード確認用
                 </label>
-                <span className="primary">必須</span>
+                <span className={passModule.primary}>必須</span>
                 <br />
                 <input
                   type="password"
                   name="confirmationPassword"
                   id="confirmationPassword"
-                  className="inputParts border border-neutral-500 rounded pl-2.5"
+                  className={`${passModule.inputParts} border border-neutral-500 rounded pl-2.5`}
                   placeholder="例：abcdef123456"
                   onChange={(e) => handleChange(e)}
                 />
@@ -397,12 +268,12 @@ export default function Password({
                 <p>※確認のためパスワードを再入力して下さい</p>
                 <br />
               </div>
-              <div className="buttonArea">
-                <button type="submit" className="submitButton">
+              <div className={passModule.buttonArea}>
+                <button type="submit" className={passModule.submitButton}>
                   パスワードを変更
                 </button>
                 {completeText && (
-                  <p className="completeText">{completeText}</p>
+                  <p className={passModule.completeText}>{completeText}</p>
                 )}
               </div>
             </form>
