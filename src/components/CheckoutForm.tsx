b9fef551eb00f8f092f603890abd321ba9a73f6f
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-17 11:44:52 +0900

    CheckoutForm 途中保存

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -1,5 +1,94 @@
+import {
+  PaymentElement,
+  LinkAuthenticationElement,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
+import { useEffect, useState } from 'react';
+import * as stripeJs from '@stripe/stripe-js';
+import { SyntheticEvent } from 'react';
+
 export default function CheckoutForm() {
-    return (
-        <></>
-    );
+  // stripeライブラリへアクセス
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [email, setEmail] = useState<string>('');
+  const [message, setMessage] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  // 支払いステータスの表示
+  useEffect(() => {
+    if (!stripe) return;
+
+    // 支払いステータスを取得
+    const clientSecret = new URLSearchParams(
+      window.location.search
+    ).get('payment_intent_client_secret');
+    if (!clientSecret) return;
+
+    stripe
+      .retrievePaymentIntent(clientSecret)
+      .then(({ paymentIntent }) => {
+        switch (paymentIntent?.status) {
+          case 'succeeded':
+            setMessage('支払いが完了しました');
+            break;
+          case 'processing':
+            setMessage('支払い処理中です');
+            break;
+          case 'requires_payment_method':
+            setMessage(
+              '支払い処理に失敗しました。お手数ですがもう一度お試しください'
+            );
+            break;
+          default:
+            setMessage('予期せぬエラーが発生しました');
+            break;
+        }
+      });
+  }, [stripe]);
+
+  const paymentElementOptions: stripeJs.StripePaymentElementOptions =
+    {
+      layout: 'tabs',
+    };
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: '/purchaseComp',
+      },
+    });
+
+    if (
+      error.type === 'card_error' ||
+      error.type === 'validation_error'
+    ) {
+      setMessage(`${error.message}`);
+    } else {
+      setMessage('予期せぬエラーが発生しました');
+    }
+  };
+
+  return (
+    <>
+      <form id="payment-form">
+        <LinkAuthenticationElement
+          id="link-authentication-element" /*onChangeイベントが必要?(targetがないといわれた)*/
+        />
+        <PaymentElement
+          id="payment-element"
+          options={paymentElementOptions}
+        />
+      </form>
+    </>
+  );
 }
