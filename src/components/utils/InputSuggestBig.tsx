c543bd306a076a2d8d5599c133bde1dcf25c2110
YukiNakajima <yuki.nakajima@rakus-partners.co.jp>
2023-03-22 13:36:14 +0900

    商品一覧のサジェストコンポーネント作成

---
+++ src/components/utils/InputSuggestBig.tsx
@@ -0,0 +1,152 @@
+import fetcher from "@/lib/fetcher";
+import { ChangeEvent } from "react";
+import useSWR from 'swr'
+
+type Input = {
+    value: string;
+    width?: string | number;
+    onChange?: (ev: ChangeEvent<HTMLInputElement>) => void;
+    onClick: (value: string) => void;
+}
+
+const InputSuggestBig = ({ value, onChange, onClick }: Input) => {
+    const input = value.split(/\s+/)[0]
+    const { data, error } = useSWR(`/api/suggest?input=${input}`, fetcher)
+    if (!data || data.length === 0 || error || data[0].name === value) {
+        if (value) {
+            return (
+                <div className="relative">
+                    <input
+                        type='text'
+                        placeholder='何をお探しですか？'
+                        value={value}
+                        onChange={onChange}
+                    />
+                    <button
+                        type="button"
+                        onClick={() => onClick('')}
+                    >
+                        x
+                    </button>
+                    <style jsx>{`
+                        input {
+                            height: 40px;
+                            width: 600px;
+                            border: 1.5px solid #574142;
+                            border-radius: 0.25rem;
+                            padding-left: 10px;
+                        }
+                        button {
+                            display: inline-block;
+                            position: absolute;
+                            height: 40px;
+                            width: 40px;
+                            left: 559px;
+                            background-color: #ffffff;
+                            border-top-width: 1.5px; 
+                            border-bottom-width: 1.5px;
+                            border-top-right-radius: 0.25rem;
+                            border-bottom-right-radius: 0.25rem;
+                            border-color: #574142;
+                        }
+                    `}</style>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <input
+                        type='text'
+                        placeholder='何をお探しですか？'
+                        value={value}
+                        onChange={onChange}
+                    />
+                    <style jsx>{`
+                        input {
+                            height: 40px;
+                            width: 600px;
+                            border: 1.5px solid #574142;
+                            border-radius: 0.25rem;
+                            padding-left: 10px;
+                        }
+                    `}</style>
+                </div>
+            )
+        }
+    } else {
+        if (value) {
+            return (
+                <div className="relative">
+                    <input
+                        type='text'
+                        placeholder='何をお探しですか？'
+                        value={value}
+                        onChange={onChange}
+                    />
+                    <div className="suggest">
+                        {data.map((item, index) => {
+                            return (
+                                <div
+                                    key={`suggest_${index}`}
+                                    className='suggest-list'
+                                    onClick={() => onClick(item.name)}
+                                >
+                                    {item.name}
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <style jsx>{`
+                            input {
+                                height: 40px;
+                                width: 600px;
+                                border: 1.5px solid #574142;
+                                border-top-left-radius: 0.25rem;
+                                border-top-right-radius: 0.25rem;
+                                padding-left: 10px;
+                            }
+                            .suggest {
+                                position: absolute;
+                                z-index: 50;
+                            }
+                            .suggest-list {
+                                width: 600px;
+                                border-width: 1px;
+                                background-color: rgb(255 255 255);
+                                border-color: #574142;
+                                font-size: 14px;
+                                line-height: 20px;
+                                padding: 10px;
+                                cursor: pointer;
+                            }
+                            .suggest-list:hover {
+                                background-color: rgb(229 229 229);
+                            }
+                    `}</style>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <input
+                        type='text'
+                        placeholder='何をお探しですか？'
+                        value={value}
+                        onChange={onChange}
+                    />
+                    <style jsx>{`
+                        input {
+                            height: 40px;
+                            width: 600px;
+                            border: 1.5px solid #574142;
+                            border-radius: 0.25rem;
+                            padding-left: 10px;
+                        }
+                    `}</style>
+                </div>
+            )
+        }
+    }
+}
+
+export default InputSuggestBig;
