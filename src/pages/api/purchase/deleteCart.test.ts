83f985a3df7becab82e051b7b81f6c388ec21a72
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-23 16:39:36 +0900

    deleteCart.tsのテスト完了

--- src/pages/api/purchase/deleteCart.test.ts
+++ src/pages/api/purchase/deleteCart.test.ts
@@ -1,1 +1,42 @@
-import handler from './deleteCart';
+import { resData } from '@/types/types';
+import { NextApiRequest, NextApiResponse } from 'next';
+import handler from './addHistory';
+
+// モックのリクエストとレスポンスを定義
+const mockReq: NextApiRequest = {
+  method: 'DELETE',
+  query: {
+    id: '1',
+  },
+} as unknown as NextApiRequest;
+
+const mockRes: NextApiResponse<resData> = {
+  status: jest.fn(() => mockRes),
+  json: jest.fn(),
+} as unknown as NextApiResponse<resData>;
+
+jest.clearAllMocks();
+describe('api/purchase/addHistory.tsのテスト', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  test('正常なレスポンス', async () => {
+    global.fetch = jest.fn().mockResolvedValueOnce({ ok: true });
+    await handler(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.json).toHaveBeenCalledWith({ message: 'OK' });
+  });
+
+  test('異常なレスポンス', async () => {
+    global.fetch = jest.fn().mockResolvedValueOnce({ ok: false });
+    await handler(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(401);
+    expect(mockRes.json).toHaveBeenCalledWith({ message: 'Failed' });
+  });
+});
