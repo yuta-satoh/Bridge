6ebff890a2d6e778bfe221dc7f24a9e19912d347
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-23 17:29:46 +0900

    userRegister/index.tsのテスト完了

---
+++ src/pages/api/userResister/index.test.ts
@@ -0,0 +1,51 @@
+import { resData } from '@/types/types';
+import { NextApiRequest, NextApiResponse } from 'next';
+import handler from './index';
+
+// モックのリクエストとレスポンスを定義
+const mockReq: NextApiRequest = {
+    method: 'POST',
+    body: {
+        id: 1,
+        lastname: '山田',
+        firstname: '太郎',
+        gender: 'male',
+        tell: '111-111-1111',
+        email: 'hogge@example.com',
+        zipcode: '111-1111',
+        address: '日本',
+        password: 'hoge1234',
+        delete: false,
+    }
+  } as NextApiRequest;
+  
+  const mockRes: NextApiResponse<resData> = {
+    status: jest.fn(() => mockRes),
+    json: jest.fn(),
+  } as unknown as NextApiResponse<resData>;
+  
+  jest.clearAllMocks();
+  describe('api/purchase/addHistory.tsのテスト', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+    afterAll(() => {
+      jest.clearAllMocks();
+    });
+  
+    test('正常なレスポンス', async () => {
+      global.fetch = jest.fn().mockResolvedValueOnce({ ok: true });
+      await handler(mockReq, mockRes);
+  
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith({ message: 'OK' });
+    });
+  
+    test('異常なレスポンス', async () => {
+      global.fetch = jest.fn().mockResolvedValueOnce({ ok: false });
+      await handler(mockReq, mockRes);
+  
+      expect(mockRes.status).toHaveBeenCalledWith(401);
+      expect(mockRes.json).toHaveBeenCalledWith({ message: 'Registration failed.' });
+    });
+  });
