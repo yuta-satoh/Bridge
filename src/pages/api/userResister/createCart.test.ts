1fe16eb706ac0c9c7cb5bbdb5bd2ed91fcec14bb
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-23 17:19:28 +0900

    createCart.tsのテスト完了 また、テスト結果に合わせてファイルを修正

---
+++ src/pages/api/userResister/createCart.test.ts
@@ -0,0 +1,70 @@
+import { resData } from '@/types/types';
+import { NextApiRequest, NextApiResponse } from 'next';
+import handler from './createCart';
+
+// モックのリクエストとレスポンスを定義
+const mockReq: NextApiRequest = {
+  method: 'POST',
+  query: {
+    email: 'hoge@example.com',
+  },
+} as unknown as NextApiRequest;
+
+const mockRes: NextApiResponse<resData> = {
+  status: jest.fn(() => mockRes),
+  json: jest.fn(),
+} as unknown as NextApiResponse<resData>;
+
+// ダミーのユーザーID
+const dummy_usersID = '1';
+
+jest.clearAllMocks();
+describe('api/purchase/addHistory.tsのテスト', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  test('正常なレスポンス', async () => {
+    global.fetch = jest
+      .fn()
+      .mockResolvedValueOnce({
+        ok: true,
+        json: () => Promise.resolve(dummy_usersID),
+      })
+      .mockResolvedValueOnce({ ok: true });
+
+    await handler(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.json).toHaveBeenCalledWith({ message: 'OK' });
+  });
+
+  test('users_idの取得に失敗', async () => {
+    global.fetch = jest
+      .fn()
+      .mockResolvedValueOnce({ ok: false });
+
+    await handler(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(401);
+    expect(mockRes.json).toHaveBeenCalledWith({ message: 'User Data was nothing.' });
+  });
+
+  test('新しいcartsデータの生成に失敗', async () => {
+    global.fetch = jest
+      .fn()
+      .mockResolvedValueOnce({
+        ok: true,
+        json: () => Promise.resolve(dummy_usersID),
+      })
+      .mockResolvedValueOnce({ ok: false });
+
+    await handler(mockReq, mockRes);
+
+    expect(mockRes.status).toHaveBeenCalledWith(401);
+    expect(mockRes.json).toHaveBeenCalledWith({ message: 'Creating cart data was wrong.' });
+  });
+});
--- src/pages/api/userResister/createCart.ts
+++ src/pages/api/userResister/createCart.ts
@@ -1,35 +1,39 @@
+import { resData } from '@/types/types';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<resData>
 ) {
-	// const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYXBpX3VzZXIifQ.OOP7yE5O_2aYFQG4bgMBQ9r0f9sikNqXbhJqoS9doTw";
-
     const email = req.query.email
 
-    const uid: { id: number }[] = await (await fetch(`${process.env.SUPABASE_URL}/users?select=id&email=eq.${email}`, {
+    const userRes = await fetch(`${process.env.SUPABASE_URL}/users?select=id&email=eq.${email}`, {
         method: "GET",
         headers: {
             "apikey": `${process.env.SUPABASE_API_KEY}`,
             "Authorization": `Bearer ${process.env.SUPABASE_API_KEY}`,
             "Content-Type": "application/json",
         },
-    })).json()
-  
-    const response = await fetch(`${process.env.SUPABASE_URL}/carts`, {
-        method: req.method,
-        headers: {
-            "apikey": `${process.env.SUPABASE_API_KEY}`,
-            "Authorization": `Bearer ${process.env.SUPABASE_API_KEY}`,
-            "Content-Type": "application/json",
-        },
-            body: JSON.stringify({ user_id: uid[0].id })
-        })
+    });
 
-    if (response.ok) {
-        res.status(201).end()
+    if(userRes.ok) {
+        const uid: { id: number }[] = await userRes.json();
+        const cartRes = await fetch(`${process.env.SUPABASE_URL}/carts`, {
+            method: req.method,
+            headers: {
+                "apikey": `${process.env.SUPABASE_API_KEY}`,
+                "Authorization": `Bearer ${process.env.SUPABASE_API_KEY}`,
+                "Content-Type": "application/json",
+            },
+                body: JSON.stringify({ user_id: uid[0].id })
+            })
+    
+        if (cartRes.ok) {
+            res.status(200).json({message: "OK"});
+        } else {
+            res.status(401).json({message: "Creating cart data was wrong."});
+        }
     } else {
-        res.status(400).json({message: "エラー"})
+        res.status(401).json({message: "User Data was nothing."});
     }
 }
