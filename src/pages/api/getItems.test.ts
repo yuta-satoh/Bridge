bd625ef4f0e3bcc967c8a80da7cc5709edeca80e
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-24 11:40:11 +0900

    getItems.tsのテスト完了

---
+++ src/pages/api/getItems.test.ts
@@ -0,0 +1,60 @@
+import handler from './getItems';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Items, resData } from '@/types/types';
+
+// リクエストとレスポンスをモック
+const mockReq: NextApiRequest = {
+    method: 'GET',
+    query: {
+        genre: 'hoge',
+        category: 'hoge',
+    }
+} as unknown as NextApiRequest;
+
+const mockRes: NextApiResponse<resData | Items[]> = {
+    status: jest.fn(() => mockRes),
+    json: jest.fn(),
+} as unknown as NextApiResponse<resData | Items[]>;
+
+// ダミーの商品データ
+const dummy_items: Items[] =[{
+    id: 1,
+    name: 'hoge',
+    description: 'hoge',
+    genre: 'hoge',
+    category: 'hoge',
+    price: 1000,
+    imgurl: 'hoge',
+    stock: 50,
+    delete: false,
+}]
+
+describe('api/getItemListData.tsのテスト', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+    afterAll(() => {
+        jest.clearAllMocks();
+    });
+
+    test('正常なレスポンス', async () => {
+        global.fetch = jest.fn().mockResolvedValueOnce({
+            ok: true,
+            json: () => Promise.resolve(dummy_items),
+        });
+        await handler(mockReq, mockRes);
+
+        expect(mockRes.status).toBeCalledWith(200);
+        expect(mockRes.json).toBeCalledWith(dummy_items);
+    });
+
+    test('正常なレスポンス', async () => {
+        global.fetch = jest.fn().mockResolvedValueOnce({
+            ok: false,
+        });
+        await handler(mockReq, mockRes);
+
+        expect(mockRes.status).toBeCalledWith(401);
+        expect(mockRes.json).toBeCalledWith({ message: 'Getting Items was failed.' });
+    });
+});
