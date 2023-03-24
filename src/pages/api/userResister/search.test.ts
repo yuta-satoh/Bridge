458dc5fb6a78370fd978094e918592b3a63b85c1
kobahaya <hayato.kobayashi@rakus-partners.co.jp>
2023-03-24 09:34:41 +0900

    search.tsのテスト完了

--- src/pages/api/userResister/search.test.ts
+++ src/pages/api/userResister/search.test.ts
@@ -5,24 +5,59 @@ import searchUser from '@/lib/searchUser';
 
 // モックのリクエストとレスポンスを定義
 const mockReq: NextApiRequest = {
-    method: 'GET',
-    body: {
-        email: 'hoge@example.com',
-    }
+  method: 'GET',
+  body: {
+    email: 'hoge@example.com',
+  },
 } as NextApiRequest;
 
 const mockRes: NextApiResponse<resData> = {
-    status: jest.fn(() => mockRes),
-    json: jest.fn(),
+  status: jest.fn(() => mockRes),
+  json: jest.fn(),
 } as unknown as NextApiResponse<resData>;
 
+// searchUser関数のモック
+jest.mock('@/lib/searchUser', () => jest.fn());
+
 describe('api/userRegister/search.tsのテスト', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
-    afterAll(() => {
-        jest.clearAllMocks();
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  test('メールアドレス重複あり', async () => {
+    // seachUser => 'このメールアドレスはすでに使用されています'
+    (
+      searchUser as jest.MockedFunction<typeof searchUser>
+    ).mockResolvedValueOnce({
+      email: 'このメールアドレスはすでに使用されています',
     });
 
-    test('メールアドレス')
+    await handler(mockReq, mockRes);
+
+    expect(searchUser).toBeCalledWith(mockReq.body.email);
+    expect(mockRes.status).toBeCalledWith(200);
+    expect(mockRes.json).toBeCalledWith({
+      email: 'このメールアドレスはすでに使用されています',
+    });
+  });
+
+  test('メールアドレス重複なし', async () => {
+    // seachUser => ''(empty)
+    (
+      searchUser as jest.MockedFunction<typeof searchUser>
+    ).mockResolvedValueOnce({
+      email: '',
+    });
+
+    await handler(mockReq, mockRes);
+
+    expect(searchUser).toBeCalledWith(mockReq.body.email);
+    expect(mockRes.status).toBeCalledWith(200);
+    expect(mockRes.json).toBeCalledWith({
+      email: '',
+    });
+  });
 });
