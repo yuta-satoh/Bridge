1ce00311d62758a957dbfe89eea53d519516799c
yuta-satoh <yuta.sato@rakus-partners.co.jp>
2023-03-28 15:15:38 +0900

    fix

--- src/pages/api/logoutIron.ts
+++ src/pages/api/logoutIron.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default withIronSessionApiRoute(handler, sessionOptions);
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  req.session.user = { user: `` };
+  req.session.user = { user: '0' };
   await req.session.save();
   res.send({ ok: true });
 }
